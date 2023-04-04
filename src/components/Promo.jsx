e9f05e32ab0eb7d04a4551bc580182859d551e43
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-04-04 09:23:09 +0100

    promo component

---
+++ src/components/Promo.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Promo = () => {
+  return (
+    <Wrapper>
+      <div className='promo-wrapper'>
+        <div className='left card'>
+          <div className='card-content'>
+            <h3>peace of mind</h3>
+            <p>
+              A one-stop platform for all your fashion needs, hassle-free. <br />Buy
+              with a peace of mind.
+            </p>
+            <a href='#new_arrival'>Buy now</a>
+          </div>
+        </div>
+        <div className='right card'>
+          <div className='card-content'>
+            <h3>Buy 2 Get 1 Free</h3>
+            <p>
+              End of season sale. Buy any 2 items of your <br />choice and get 1 free.
+            </p>
+            <a href='#new_arrival'>Buy now</a>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  padding: 4rem 1rem;
+
+  .promo-wrapper {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 1.5rem;
+
+    .card {
+      background: #000000;
+      min-height: 26.25rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #ffffff;
+      padding: 1rem;
+
+      .card-content {
+        text-align: center;
+        font-family: 'Lato', sans-serif;
+        h3 {
+          text-transform: uppercase;
+          font-style: normal;
+          font-weight: 300;
+          font-size: 2rem;
+          line-height: 48px;
+        }
+        p {
+          font-weight: 400;
+          font-size: 1rem;
+          line-height: 130%;
+          padding: 1.4rem 0;
+        }
+
+        a {
+          background: #ffffff;
+          box-shadow: 0px 6px 15px rgba(0, 0, 0, 0.04);
+          color: #024e82;
+          font-size: 14px;
+          text-decoration: none;
+          padding: 1rem 2rem;
+          display: inline-block;
+          font-weight: 500;
+          text-transform: uppercase;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 480px) {
+    padding: 4rem 2rem;
+
+    .promo-wrapper {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  @media screen and (min-width: 780px) {
+    padding: 4rem;
+
+    .promo-wrapper {
+      grid-template-columns: 1.5fr 1fr;
+    }
+  }
+`
+
+export default Promo
