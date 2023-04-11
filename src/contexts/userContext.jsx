50090e5008dd3540d0152a35611a2b5894ebefed
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-04-11 15:30:32 +0100

    feat: add user context, and userreducer for use authentication

---
+++ src/contexts/userContext.jsx
@@ -0,0 +1,29 @@
+import { useContext, createContext, useReducer } from 'react'
+import userReducer from '../reducers/userReducer'
+
+const UserContext = createContext()
+
+const initialState = {
+  authenticated: false,
+  user: {},
+}
+
+export const UserProvider = ({ children }) => {
+  const [state, dispath] = useReducer(userReducer, initialState)
+
+  const handleLogin = (email, password) => {
+    console.log(email, password)
+  }
+
+  const handleRegister = (name, email, password)=> {
+    console.log(name, email, password)
+  }
+
+  return (
+    <UserContext.Provider value={{ ...state, handleLogin, handleRegister }}>{children}</UserContext.Provider>
+  )
+}
+
+export const useUserContext = () => {
+  return useContext(UserContext)
+}
