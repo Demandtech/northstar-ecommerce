4833eafb94d6ed61e88e221b379536dd88248612
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-04-15 06:58:40 +0100

    feat: add user setting component

---
+++ src/components/UserSetting.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import {FaSignOutAlt} from 'react-icons/fa'
+import { useUserContext } from '../contexts/userContext'
+
+const UserSetting = () => {
+ const { handleLogout } = useUserContext()
+ console.log(handleLogout)
+  return (
+    <Wrapper>
+      <Link className='link-btn' to={'/profile'}>Profile</Link>
+      <Link className='link-btn' to={'/cart'}>Order</Link>
+      <button className='logout-btn' onClick={handleLogout}>
+        <span>Sign Out</span>
+        <FaSignOutAlt />
+      </button>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.article`
+  position: absolute;
+  background-color: #ffffff;
+  min-width: 130px;
+  padding: 1rem;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  font-size: 14px;
+  text-transform: uppercase;
+
+  .logout-btn {
+    all: unset;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    color: red;
+    opacity: 0.5;
+    font-weight: 500;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  .link-btn {
+    color: #b1b1b1;
+    text-decoration: none;
+    font-weight: 500;
+    transition: .3s;
+
+    &:hover {
+      color: #024e82;
+    }
+  }
+`
+
+export default UserSetting
